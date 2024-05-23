2af1fb6fb3c4f3fe3b6fc89735e170e7e01aede4
DFran49 <francresmartin@gmail.com>
2024-05-23 19:40:24 +0200

    Proyecto CodeSphere - fix css; add script menú móvil

---
+++ Lenguajes de marcas/3er trimestre/proyecto/js/mostrar-esconder.js
@@ -0,0 +1,53 @@
+function inicio() {
+    document.getElementById('iniMovil').style.display = 'flex';
+    document.getElementById('buscMovil').style.display = 'none';
+    document.getElementById('msgMovil').style.display = 'none';
+    document.getElementById('chatMovil').style.display = 'none';
+    document.getElementById('perfilMovil').style.display = 'none';
+    document.getElementById('ajustesMovil').style.display = 'none';
+}
+
+function buscar() {
+    document.getElementById('iniMovil').style.display = 'none';
+    document.getElementById('buscMovil').style.display = 'flex';
+    document.getElementById('msgMovil').style.display = 'none';
+    document.getElementById('chatMovil').style.display = 'none';
+    document.getElementById('perfilMovil').style.display = 'none';
+    document.getElementById('ajustesMovil').style.display = 'none';
+}
+
+function mensajes() {
+    document.getElementById('iniMovil').style.display = 'none';
+    document.getElementById('buscMovil').style.display = 'none';
+    document.getElementById('msgMovil').style.display = 'flex';
+    document.getElementById('chatMovil').style.display = 'none';
+    document.getElementById('perfilMovil').style.display = 'none';
+    document.getElementById('ajustesMovil').style.display = 'none';
+}
+
+function chat() {
+    document.getElementById('iniMovil').style.display = 'none';
+    document.getElementById('buscMovil').style.display = 'none';
+    document.getElementById('msgMovil').style.display = 'none';
+    document.getElementById('chatMovil').style.display = 'flex';
+    document.getElementById('perfilMovil').style.display = 'none';
+    document.getElementById('ajustesMovil').style.display = 'none';
+}
+
+function perfil() {
+    document.getElementById('iniMovil').style.display = 'none';
+    document.getElementById('buscMovil').style.display = 'none';
+    document.getElementById('msgMovil').style.display = 'none';
+    document.getElementById('chatMovil').style.display = 'none';
+    document.getElementById('perfilMovil').style.display = 'flex';
+    document.getElementById('ajustesMovil').style.display = 'none';
+}
+
+function ajustes() {
+    document.getElementById('iniMovil').style.display = 'none';
+    document.getElementById('buscMovil').style.display = 'none';
+    document.getElementById('msgMovil').style.display = 'none';
+    document.getElementById('chatMovil').style.display = 'none';
+    document.getElementById('perfilMovil').style.display = 'none';
+    document.getElementById('ajustesMovil').style.display = 'flex';
+}

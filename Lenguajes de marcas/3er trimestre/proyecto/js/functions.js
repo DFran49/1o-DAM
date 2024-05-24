a54045662a047d5ef5cd37283874baa1c84606d7
DFran49 <francresmartin@gmail.com>
2024-05-24 15:04:49 +0200

    CodeSphere - Posicionamiento móvil terminado, scripts y normalización

--- Lenguajes de marcas/3er trimestre/proyecto/js/functions.js
+++ Lenguajes de marcas/3er trimestre/proyecto/js/functions.js
@@ -1,60 +1,68 @@
+function logIn() {
+    let x = document.forms["formLogIn"]["username"].value;
+    /*if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    } else {*/
+        document.getElementById('logIn').style.display = 'none';
+        document.getElementById('contenido').style.display = 'flex';
+        document.getElementById('footerMovil').style.display = 'flex';
+    /*}*/
+}
+
 function inicio() {
-    document.getElementById('iniMovil').style.display = 'flex';
-    document.getElementById('buscMovil').style.display = 'none';
-    document.getElementById('msgMovil').style.display = 'none';
-    document.getElementById('chatMovil').style.display = 'none';
-    document.getElementById('perfilMovil').style.display = 'none';
-    document.getElementById('ajustesMovil').style.display = 'none';
+    document.getElementById('contenido').style.display = 'flex';
+    document.getElementById('buscar').style.display = 'none';
+    document.getElementById('mensajes').style.display = 'none';
+    document.getElementById('chat').style.display = 'none';
+    document.getElementById('perfil').style.display = 'none';
+    document.getElementById('ajustes').style.display = 'none';
 }
 
 function buscar() {
-    document.getElementById('iniMovil').style.display = 'none';
-    document.getElementById('buscMovil').style.display = 'flex';
-    document.getElementById('msgMovil').style.display = 'none';
-    document.getElementById('chatMovil').style.display = 'none';
-    document.getElementById('perfilMovil').style.display = 'none';
-    document.getElementById('ajustesMovil').style.display = 'none';
+    document.getElementById('contenido').style.display = 'none';
+    document.getElementById('buscar').style.display = 'flex';
+    document.getElementById('mensajes').style.display = 'none';
+    document.getElementById('chat').style.display = 'none';
+    document.getElementById('perfil').style.display = 'none';
+    document.getElementById('ajustes').style.display = 'none';
 }
 
 function mensajes() {
-    document.getElementById('iniMovil').style.display = 'none';
-    document.getElementById('buscMovil').style.display = 'none';
-    document.getElementById('msgMovil').style.display = 'flex';
-    document.getElementById('chatMovil').style.display = 'none';
-    document.getElementById('perfilMovil').style.display = 'none';
-    document.getElementById('ajustesMovil').style.display = 'none';
+    document.getElementById('contenido').style.display = 'none';
+    document.getElementById('buscar').style.display = 'none';
+    document.getElementById('mensajes').style.display = 'flex';
+    document.getElementById('chat').style.display = 'none';
+    document.getElementById('perfil').style.display = 'none';
+    document.getElementById('ajustes').style.display = 'none';
 }
 
 function chat() {
-    document.getElementById('iniMovil').style.display = 'none';
-    document.getElementById('buscMovil').style.display = 'none';
-    document.getElementById('msgMovil').style.display = 'none';
-    document.getElementById('chatMovil').style.display = 'flex';
-    document.getElementById('perfilMovil').style.display = 'none';
-    document.getElementById('ajustesMovil').style.display = 'none';
+    document.getElementById('mensajes').style.display = 'none';
+    document.getElementById('chat').style.display = 'flex';
 }
 
 function perfil() {
-    document.getElementById('iniMovil').style.display = 'none';
-    document.getElementById('buscMovil').style.display = 'none';
-    document.getElementById('msgMovil').style.display = 'none';
-    document.getElementById('chatMovil').style.display = 'none';
-    document.getElementById('perfilMovil').style.display = 'flex';
-    document.getElementById('ajustesMovil').style.display = 'none';
+    document.getElementById('contenido').style.display = 'none';
+    document.getElementById('buscar').style.display = 'none';
+    document.getElementById('mensajes').style.display = 'none';
+    document.getElementById('chat').style.display = 'none';
+    document.getElementById('perfil').style.display = 'flex';
+    document.getElementById('ajustes').style.display = 'none';
 }
 
 function ajustes() {
-    document.getElementById('iniMovil').style.display = 'none';
-    document.getElementById('buscMovil').style.display = 'none';
-    document.getElementById('msgMovil').style.display = 'none';
-    document.getElementById('chatMovil').style.display = 'none';
-    document.getElementById('perfilMovil').style.display = 'none';
-    document.getElementById('ajustesMovil').style.display = 'flex';
+    document.getElementById('contenido').style.display = 'none';
+    document.getElementById('buscar').style.display = 'none';
+    document.getElementById('mensajes').style.display = 'none';
+    document.getElementById('chat').style.display = 'none';
+    document.getElementById('perfil').style.display = 'none';
+    document.getElementById('ajustes').style.display = 'flex';
 }
 
 function comentarios() {
     if (document.getElementById('comentariosMovil').style.display == 'none') {
-        document.getElementById('comentariosMovil').style.display = 'none';
+        document.getElementById('comentariosMovil').style.display = 'flex';
         alert('b');
     }
     
@@ -81,15 +89,87 @@ function cambiarGuardado() {
     }   
 }
 
-function findUsers(event) {
-    if (event.keyCode == 13) {
-        alert('enter')
-    } else {
-        alert(event.keyCode);
+function busqueda(a,b) {
+    console.log(a);
+    console.log(b);
+    var input, div, p, a, i;
+    input = a;
+    div = b;
+    p = div.getElementsByTagName("p");
+    for (i = 0; i < p.length; i++) {     
+        a = p[i].textContent;
+        if (a.indexOf(input) > -1) {
+            p[i].style.display = "";
+        } else {
+            p[i].style.display = "none";
+        }
     }
     
 }
 
-function saludo() {
-    alert();
+function buscarPerfiles(event) {
+    if (event.keyCode == 13) {
+        alert('enter')
+    } else {
+        var input, div;
+        input =  document.forms["busquedaMovil"]["buscarPerfiles"].value;
+        div = document.getElementById("listaUsers");
+        busqueda(input, div);
+    }
+    alert('a');
+}
+
+function hola(event) {
+    if (event.keyCode == 13) {
+        alert('a');
+    } else {
+        var input, div;
+        input =  document.forms["busquedaMovil"]["buscarPerfiles"].value;
+        div = document.getElementById("listaUsers");
+        busqueda()
+    }
+    alert(event.keyCode);
+}
+
+function volverMensajes() {
+    document.getElementById('mensajes').style.display = 'flex';
+    document.getElementById('chat').style.display = 'none';
+}
+
+function cambiarNombre() {
+    document.getElementById('contenedorAjustes').style.display = 'none';
+    document.getElementById('topAjustes').style.display = 'none';
+    document.getElementById('cambiarNombre').style.display = 'flex';
+}
+
+function borrarPubli() {
+    document.getElementById('contenedorAjustes').style.display = 'none';
+    document.getElementById('topAjustes').style.display = 'none';
+    document.getElementById('borrarPublicaciones').style.display = 'flex';
+}
+
+function borrarAmigos() {
+    document.getElementById('contenedorAjustes').style.display = 'none';
+    document.getElementById('topAjustes').style.display = 'none';
+    document.getElementById('borrarAmigos').style.display = 'flex';
+}
+
+function borrarCuenta() {
+    document.getElementById('contenedorAjustes').style.display = 'none';
+    document.getElementById('topAjustes').style.display = 'none';
+    document.getElementById('borrarCuenta').style.display = 'flex';
+}
+
+function logOut() {
+    location.reload();
+}
+
+function volverAjustes() {
+    document.getElementById('contenedorAjustes').style.display = 'flex';
+    document.getElementById('topAjustes').style.display = 'flex';
+    document.getElementById('cambiarNombre').style.display = 'none';
+    document.getElementById('borrarPublicaciones').style.display = 'none';
+    document.getElementById('borrarAmigos').style.display = 'none';
+    document.getElementById('borrarCuenta').style.display = 'none';
+    document.getElementById('logOutMovil').style.display = 'none';
 }
